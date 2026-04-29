84243bedcedae799c8e302fbee954a46810aae7b
Smilhy <smilhytv@gmail.com>
2026-04-29 03:42:26 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -4000,7 +4000,7 @@ function App() {
   }
 
   return (
-    <div className="app-shell">
+    <div className={`app-shell ${view === 'adminPayouts' ? 'no-rightbar-page' : ''}`}>
       <Toast toast={toast} onClose={() => setToast(null)} />
       <ProfileSubscriptionModal tip={selectedProfileSub} user={sessionUser} onClose={() => setSelectedProfileSub(null)} />
       <PaymentModal
@@ -4170,7 +4170,7 @@ function App() {
         )}
       </main>
 
-      <Rightbar ranking={realRanking} />
+      {view !== 'adminPayouts' && <Rightbar ranking={realRanking} />}
     </div>
   )
 }
